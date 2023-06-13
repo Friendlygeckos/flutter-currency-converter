a48d73c8671a26329ea6e1f89793499e1f772305
Ryan Bharath <dbossman60@gmail.com>
2023-06-13 16:18:53 -0400

    chore: renamed the title on MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Currency Converter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
